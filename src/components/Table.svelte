b71289e1dd7910f6a45473c7d48a597ee55b25a2
Sergei Ryzhko <Siarhei Ryzhko>
2025-02-18 09:41:43 +0300

    feat: implmenet table component

---
+++ src/components/Table.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { TabulatorFull as Tabulator } from 'tabulator-tables';
+	import 'tabulator-tables/dist/css/tabulator_bootstrap5.min.css';
+
+	let { columns, data, ...props } = $props();
+
+	function tableAction(node, { data, columns }) {
+		const defaultProps = {
+			pagination: 'local',
+			paginationSize: 7,
+		};
+		let table = new Tabulator(node, {
+			data,
+			columns,
+			...defaultProps,
+			...props,
+		});
+		return {
+			update: ({ data }) => {
+				table.replaceData(data);
+			}
+		};
+	}
+</script>
+
+<div use:tableAction={{ data, columns }}></div>
