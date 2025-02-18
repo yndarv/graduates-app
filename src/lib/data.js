8caa70530001f197de1e78c7abefa27ca4885186
Sergei Ryzhko <Siarhei Ryzhko>
2025-02-18 14:55:35 +0300

    dont use class for defining models

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,15 +1,74 @@
 import { mockStudent } from './mock';
 
-export const columns = [
+export const tableColumns = [
 	{
 		field: 'id',
 		title: 'ID',
 		sorter: 'number',
 		width: 50,
-		hidden: true,
+		visible: false,
 		hozAlign: 'center',
 		frozen: true,
-		editable: false
+		editable: false,
+		resizeable: false
+	},
+	{
+		field: 'fullName',
+		title: 'ФИО',
+		sorter: 'string',
+		hozAlign: 'left',
+		frozen: true,
+		resizeable: false
+	},
+	{
+		field: 'address',
+		title: 'Адрес',
+		sorter: 'string',
+		hozAlign: 'left',
+		frozen: false,
+		resizeable: false
+	},
+	{
+		field: 'phoneNumber',
+		title: 'Телефон',
+		sorter: 'alphanum',
+		hozAlign: 'center',
+		resizeable: false
+	},
+	{
+		field: 'speciality',
+		title: 'Специальность',
+		sorter: 'string',
+		hozAlign: 'left',
+	},
+	{
+	  field: 'specialityCode',
+		title: 'Код специальности',
+		sorter: 'alphanum',
+		hozAlign: 'center',
+	},
+	{
+		field: 'faculty',
+		title: 'Факультет',
+		sorter: 'string',
+		width: 120,
+	},
+	{
+		field: 'formEducation',
+		title: 'Форма образования',
+		sorter: 'string',
+	},
+	{
+		field: 'stepEducation',
+		title: 'Ступень образования',
+		sorter: 'string',
+	},
+	{
+		field: 'yearOfGraduation',
+		title: 'Год выпуска',
+		sorter: 'number',
+		hozAlign: 'center',
 	}
 ];
-export const data = Array.from({ length: 100 }, () => mockStudent());
+
+export const tableData = Array.from({ length: 100 }, () => mockStudent());
--- src/lib/mock.js
+++ src/lib/mock.js
@@ -1,5 +1,4 @@
 import { fakerRU as faker } from '@faker-js/faker';
-import { Student } from './models/student';
 
 let id = 1;
 
@@ -8,16 +7,16 @@ export function mockStudent() {
 	const formEducationOptions = ['бюджет', 'платно'];
 	const stepEducationOptions = ['первая ступень', 'вторая ступень'];
 
-	return new Student({
+	return {
 		id: id++,
 		fullName: faker.person.fullName(),
 		address: faker.location.streetAddress(),
 		phoneNumber: faker.phone.number(),
 		speciality: faker.person.jobTitle(),
-		specialtyCode: faker.number.int({ min: 100, max: 1000 }),
+		specialityCode: faker.number.int({ min: 100, max: 1000 }),
 		faculty: faker.helpers.arrayElement(faculties),
 		formEducation: faker.helpers.arrayElement(formEducationOptions),
 		stepEducation: faker.helpers.arrayElement(stepEducationOptions),
 		yearOfGraduation: faker.date.past({ years: 10 }).getFullYear()
-	});
+	};
 }
