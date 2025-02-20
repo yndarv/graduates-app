248e8febae7500489555a7ece742b5f8317c0681
Siarhei Ryzhko <yndarven@gmail.com>
2025-02-21 02:10:41 +0300

    cols for unemployed table

---
+++ src/routes/unemployed/columns.js
@@ -0,0 +1,145 @@
+export const columns = [
+	{
+		field: 'id',
+		title: 'ID',
+		visible: false
+	},
+	{
+		field: 'studentId',
+		title: 'studentId',
+		visible: false
+	},
+	{
+		field: 'fullName',
+		title: 'ФИО',
+		sorter: 'string',
+		hozAlign: 'left',
+		frozen: true,
+		editor: 'input',
+		editorParams: {},
+		headerFilter: true,
+		headerFilterPlaceholder: 'поиск',
+		editable: true
+		// validator: 'required'
+	},
+	{
+		field: 'address',
+		title: 'Адрес',
+		sorter: 'string',
+		hozAlign: 'left',
+		editor: 'input',
+		editorParams: {},
+		frozen: false,
+		headerFilter: true,
+		headerFilterPlaceholder: 'поиск',
+		editable: true
+		// validator: 'required'
+	},
+	{
+		field: 'profiling',
+		title: 'Профиль',
+		sorter: 'string',
+		hozAlign: 'left',
+		editor: 'input',
+		editorParams: {},
+		editable: true
+	},
+	{
+		field: 'nameCompany',
+		title: 'Наименование компании',
+		sorter: 'string',
+		hozAlign: 'left',
+		editor: 'input',
+		editorParams: {},
+		editable: true
+	},
+	{
+		field: 'directionReturned',
+		title: 'Направление вернули',
+		sorter: 'boolean',
+		hozAlign: 'center',
+		editor: 'tickCross',
+		formatter: 'tickCross',
+		editorParams: {},
+		editable: true
+	},
+	{
+		field: 'admissionToHigherLevel',
+		title: 'Доступ к более высокому уровню',
+		sorter: 'boolean',
+		hozAlign: 'center',
+		editor: 'tickCross',
+		formatter: 'tickCross',
+		editorParams: {},
+		editable: true
+	},
+	{
+		field: 'companyNotified',
+		title: 'Компания уведомлена',
+		sorter: 'boolean',
+		hozAlign: 'center',
+		editor: 'tickCross',
+		formatter: 'tickCross',
+		editorParams: {},
+		editable: true
+	},
+	{
+		field: 'dataNotification',
+		title: 'Дата уведомления',
+		sorter: 'date',
+		hozAlign: 'center',
+		editor: 'date',
+		editorParams: {
+			format: 'dd.MM.yyyy'
+		},
+		formatter: 'datetime',
+		formatterParams: {
+			inputFormat: 'yyyy-MM-dd HH:ss',
+			outputFormat: 'dd/MM/yy',
+			invalidPlaceholder: '(неверный формат даты)',
+			timezone: 'Europe/Minsk'
+		},
+		editorParams: {},
+		editable: true
+	},
+	{
+		field: 'conscriptionEmployment',
+		title: 'Контракт на службу',
+		sorter: 'boolean',
+		hozAlign: 'center',
+		formatter: 'tickCross',
+		editor: 'tickCross',
+		editorParams: {},
+		editable: true
+	},
+	{
+		field: 'servesArmy',
+		title: 'Служит',
+		sorter: 'boolean',
+		hozAlign: 'center',
+		formatter: 'tickCross',
+		editor: 'tickCross',
+		editorParams: {},
+		editable: true
+	},
+	{
+		field: 'paymentOfTuitionFees',
+		title: 'Оплата учебных взносов',
+		sorter: 'boolean',
+		hozAlign: 'center',
+		formatter: 'tickCross',
+		editor: 'tickCross',
+		editorParams: {},
+		editable: true
+	},
+	{
+		field: 'notes',
+		title: 'Оплата учебных взносов',
+		sorter: 'string',
+		hozAlign: 'left',
+		editor: 'textarea',
+		formatter: 'textarea',
+		editorParams: {},
+		editable: true
+	}
+];
