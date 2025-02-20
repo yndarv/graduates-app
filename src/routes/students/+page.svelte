973a6119408548515cc9439ebc786756e1fb959d
Siarhei Ryzhko <yndarven@gmail.com>
2025-02-21 02:10:49 +0300

    add rowContextMenu

--- src/routes/students/+page.svelte
+++ src/routes/students/+page.svelte
@@ -2,15 +2,43 @@
 	import Table from '$components/Table.svelte';
 	import { columns } from './columns';
 	import { createStudent, updateStudent, removeStudent } from '$lib/api/student';
+	import { goto } from '$app/navigation';
 
 	const baseUrl = 'http://localhost:8080/api/students';
+
+	const rowContextMenu = [
+		{
+			label: 'Добавить в...',
+			menu: [
+				{
+					label: 'Не трудоустроен',
+					action: function (e, row) {
+						goto(`/unemployed?studentId=${row.getData().id}`);
+					}
+				},
+				{
+					label: 'Распределён',
+					action: function (e, row) {
+						goto(`/assigned?studentId=${row.getData().id}`);
+					}
+				},
+				{
+					label: 'Перераспределён',
+					action: function (e, row) {
+						goto(`/reassigned?studentId=${row.getData().id}`);
+					}
+				}
+			]
+		}
+	];
 
 	const tableProps = {
 		columns,
 		ajaxURL: `${baseUrl}/all`,
 		createRequest: createStudent,
 		updateRequest: updateStudent,
-		removeRequest: removeStudent
+		removeRequest: removeStudent,
+		rowContextMenu
 	};
 </script>
 
