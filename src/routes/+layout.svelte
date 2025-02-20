3d8310c3d4393d64350d938238ab7ef5b79206eb
Siarhei Ryzhko <yndarven@gmail.com>
2025-02-20 22:40:11 +0300

    add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
+	import Navbar from '$components/Navbar.svelte';
 	import 'bootstrap/dist/css/bootstrap.min.css';
+	import { page } from '$app/stores'; // SvelteKit's page store
 	let { children } = $props();
+
+	// Determine the current page based on the route
+	let currentPage = $derived($page.url.pathname.split('/')[1] || 'students');
 </script>
+
+<Navbar {currentPage} />
 
 {@render children()}
