0e305fddca1b477bfe406d2135198813e0bcb409
Siarhei Ryzhko <yndarven@gmail.com>
2025-02-20 19:10:30 +0300

    initial api for student resource

---
+++ src/lib/api/student.js
@@ -0,0 +1,23 @@
+const baseUrl = 'http://localhost:8080/api/students';
+
+export async function createStudent(student) {
+	const resp = await fetch(baseUrl, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(student)
+	}).then((res) => res.json());
+	return resp.id;
+}
+
+export async function updateStudent(id, student) {
+	const resp = await fetch(`${baseUrl}/${id}`, {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(student)
+	}).then((res) => res.json());
+	return resp.id;
+}
