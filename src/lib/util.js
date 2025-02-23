f7f9ba1837887eea42ce5dc940eedf06c35b09d9
Siarhei Ryzhko <yndarven@gmail.com>
2025-02-23 18:44:19 +0300

    add utils for jwt token

---
+++ src/lib/util.js
@@ -0,0 +1,29 @@
+export function setAuthToken({ cookies, token }) {
+	cookies.set('token', token, {
+		httpOnly: true,
+		secure: true,
+		sameSite: 'strict',
+		maxAge: 60 * 60 * 24, // 1d
+		path: '/'
+	});
+}
+
+export function decodeJwtPayload(token) {
+	try {
+		const base64Url = token.split('.')[1]; // Get the payload part of the JWT
+		const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // Convert from Base64Url to Base64
+		const jsonPayload = decodeURIComponent(
+			atob(base64)
+				.split('')
+				.map(function (c) {
+					return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+				})
+				.join('')
+		);
+
+		return JSON.parse(jsonPayload);
+	} catch (error) {
+		console.error('Error decoding JWT:', error);
+		return null;
+	}
+}
