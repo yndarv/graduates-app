223b7141dd1234580dbdb8de7cca4940144db02f
Siarhei Ryzhko <yndarven@gmail.com>
2025-02-23 19:19:27 +0300

    style navbar with bootstrap

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,70 @@
 <script>
+	import 'bootstrap/dist/css/bootstrap.min.css';
+	import { page } from '$app/stores';
+
 	let { children, data } = $props();
 	const user = data.user;
 	console.log(data.user);
+
+	async function logout() {
+		await fetch('/logout', {
+			method: 'POST'
+		});
+		window.location.href = '/login';
+	}
 </script>
 
 <header>
-	<h1>Выпускники БрГТУ</h1>
-	<nav>
-		<ul>
-			<li><a href="/students">Студенты</a></li>
-			<li><a href="/unemployed">Незанятые</a></li>
-			<li><a href="/assigned">Распределённые</a></li>
-			<li><a href="/reassigned">Перераспределённые</a></li>
-		</ul>
-		{#if data?.user}
-			<h3>Авторизованы как {user.sub}</h3>
-			<form method="POST" action="?/logout">
-				<button type="submit">Выход</button>
-			</form>
-		{:else}
-			<li><a href="/login">Вход</a></li>
-		{/if}
-		<ul></ul>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light px-3">
+		<a class="navbar-brand" href="/">
+			<h3 class="m-0">Выпускники БрГТУ</h3>
+		</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarNav"
+			aria-controls="navbarNav"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav me-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="/students" class:active={$page.url.pathname === '/students'}
+						>Студенты</a
+					>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/unemployed" class:active={$page.url.pathname === '/unemployed'}
+						>Незанятые</a
+					>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/assigned" class:active={$page.url.pathname === '/assigned'}
+						>Распределённые</a
+					>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/reassigned" class:active={$page.url.pathname === '/reassigned'}
+						>Перераспределённые</a
+					>
+				</li>
+			</ul>
+			<div class="d-flex align-items-center">
+				{#if data?.user}
+					<span class="me-3">Авторизованы как {user.sub}</span>
+					<button type="submit" class="btn btn-outline-danger" onclick={logout}>Выход</button>
+				{:else}
+					<a class="btn btn-outline-primary" href="/login">Вход</a>
+				{/if}
+			</div>
+		</div>
 	</nav>
 </header>
 
-<main>
+<main class="container mt-4">
 	{@render children()}
 </main>
