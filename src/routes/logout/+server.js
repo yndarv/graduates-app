e1d417e569ba02a0520ca3c0f415b3b769e9df40
Siarhei Ryzhko <yndarven@gmail.com>
2025-02-23 19:19:35 +0300

    add logout endpoint

---
+++ src/routes/logout/+server.js
@@ -0,0 +1,4 @@
+export function POST({ cookies }) {
+	cookies.delete('token', { path: '/' }); // Adjust based on your session handling
+	throw redirect(302, '/login'); // Redirect to login after logout
+}
