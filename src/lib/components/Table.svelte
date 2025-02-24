164f739f86d469e1a72f5da20bcd4b0a4e276278
Siarhei Ryzhko <yndarven@gmail.com>
2025-02-24 08:01:52 +0300

    Add table component

---
+++ src/lib/components/Table.svelte
@@ -0,0 +1,114 @@
+<script>
+	import { onMount } from 'svelte';
+	import { TabulatorFull as Tabulator } from 'tabulator-tables';
+	import 'tabulator-tables/dist/css/tabulator_materialize.min.css';
+	import { DateTime } from 'luxon';
+	import * as XLSX from 'xlsx';
+
+	let {
+		createRequest,
+		patchRequest,
+		deleteRequest,
+		rowContextMenu,
+		onMountHandler,
+		addRowButton,
+		exportFileName,
+		...props
+	} = $props();
+
+	const rowHeader = {
+		title: '#',
+		field: 'rownum',
+		formatter: 'rownum',
+		headerSort: false,
+		hozAlign: 'center',
+		resizable: false,
+		width: 60,
+		frozen: true
+	};
+
+	const defaultRowContextMenu = [
+		{
+			label: "<b class='text-danger'><i class='fas fa-trash'></i> Удалить</b>",
+			action: async (e, row) => {
+				await deleteRequest(row.getData().id).catch((error) => {
+					console.error('failed deleting row with number ', row.getData().id, error);
+				});
+				row.delete();
+			}
+		}
+	];
+	if (rowContextMenu) {
+		defaultRowContextMenu.push(...rowContextMenu);
+	}
+
+	let table;
+	let tableNode;
+
+	onMount(() => {
+		table = new Tabulator(tableNode, {
+			layout: 'fitDataStretch',
+			// layout: 'fitColumns',
+			placeholder: 'Нет данных',
+			reactiveData: true,
+			rowHeader,
+			rowContextMenu: defaultRowContextMenu,
+			dependencies: {
+				DateTime,
+				XLSX
+			},
+			height: '90vh',
+			...props
+		});
+
+		if (onMountHandler) {
+			onMountHandler(table);
+		}
+
+		table.on('importImported', async (data) => {
+			for (let i = 0; i < data.length; i++) {
+				const rowData = data[i];
+				const id = createRequest(rowData).id;
+				console.log('id', id);
+			}
+			// const addedRows = await table.getRows();
+			// console.log('addedRows', addedRows);
+		});
+
+		table.on('cellEdited', async (cell) => {
+			console.log(cell.getColumn().getField(), cell.getValue().toString());
+			let result = await patchRequest(cell.getRow().getData().id, {
+				[cell.getColumn().getField()]: cell.getValue().toString()
+			});
+			console.log('PATH RESULT', result);
+		});
+	});
+
+	function handleAddRow() {
+		table.addRow({}, true).then(async (row) => {
+			const { id } = await createRequest({});
+			row.update({ id });
+
+			const cellToEdit = row.getCell('fullName');
+			cellToEdit.edit();
+		});
+	}
+
+	function handleExport(type) {
+		table.download(type, exportFileName);
+	}
+
+	async function handleImport() {
+		const importedData = await table.import('xlsx', ['.xlsx', '.csv', '.ods'], 'buffer');
+		console.log('importedData', importedData);
+	}
+</script>
+
+{#if addRowButton}
+	<button class="btn btn-primary btn-sm" onclick={handleAddRow}>Добавить строку</button>
+{/if}
+
+<button class="btn btn-secondary btn-sm" onclick={handleExport('xlsx')}> Экспорт в Excel</button>
+<button class="btn btn-secondary btn-sm" onclick={handleImport}> Импорт</button>
+
+<div bind:this={tableNode}></div>
